9d22d1b94fb05131e4b9a2991652939794f77290
Hendikaaw <ah3n413ah20oiz2@gmail.com>
2026-05-16 16:05:07 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -203,7 +203,11 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server Radio berjalan di http://localhost:${PORT}`);
-});
+// Menjalankan server di lokal, atau mengekspornya untuk Vercel Serverless
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+        console.log(`Server Radio berjalan di http://localhost:${PORT}`);
+    });
+}
+module.exports = server;
